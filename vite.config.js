e0905dfd43017a6c096ceae2b577b60830840327
hjiabin <hjbmeow@163.com>
2021-09-01 09:58:45 +0800

    vite定义全局变量

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import * as path from 'path'
 import { defineConfig } from 'vite'
 // import vue from "@vitejs/plugin-vue";
 import { createVuePlugin } from "vite-plugin-vue2";
+import { version } from "./package.json";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [createVuePlugin({ jsx: true })],
@@ -11,6 +12,9 @@ export default defineConfig({
       '@': path.resolve(__dirname, './src'),
       'lib': path.resolve(__dirname, './lib'),
     }
+  },
+  define: {
+    $version: JSON.stringify(version)
   },
   build: {
     lib: {
